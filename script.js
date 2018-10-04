052b3bee44f0e7cb6e743a21432a1ff5ed6f0c36
Brumz <bmanrumsey24@gmail.com>
2018-10-04 16:50:30 -0600

    cleaned code up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("canvas")
+var canvas = document.getElementById("canvas");
 canvas.addClass = "canvas";
 
 var palette = document.getElementById("palette");
@@ -10,20 +10,20 @@ for (let i = 0; i < 2542; i++) {
    cell.addEventListener("click", fill)
 }
 
-var colorArray = ['#001f3f', '#0074D9', '#7FDBFF', '#39CCCC',
-          '#3D9970', '#2ECC40', '#01FF70', '#FFDC00', '#FF851B',
-          '#FF4136', '#85144b', '#F012BE',
-          '#B10DC9', '#111111', '#AAAAAA', '#DDDDDD', '#FFFFFF'];
+var colorArray = ["#001f3f", "#0074D9", "#7FDBFF", "#39CCCC",
+          "#3D9970", "#2ECC40", "#01FF70", "#FFDC00", "#FF851B",
+          "#FF4136", "#85144b", "#F012BE",
+          "#B10DC9", "#111111", "#AAAAAA", "#DDDDDD", "#FFFFFF"];
           
 for (let i = 0; i < colorArray.length; i++) {
    var color = document.createElement("div");
    color.classList.add("color");
    color.style.backgroundColor = colorArray[i];
-   color.addEventListener("click", penColor)
+   color.addEventListener("click", penColor);
    palette.appendChild(color);
 }
 
-var pen = color.style.backgroundColor
+var pen = color.style.backgroundColor;
 function penColor() {
    pen = event.target.style.backgroundColor;
 }
@@ -37,7 +37,7 @@ var selector = document.createElement("div");
 selector.classList.add("selector");
 selector.style.backgroundColor = pen;
 palette.appendChild(selector);
-selector.addEventListener("click",fill)
+selector.addEventListener("click",fill);
 
 
 
